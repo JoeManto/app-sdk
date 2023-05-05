fdcb28463d289e1038a032f091a88394799d9ac9
JoeManto <joemanto123@gmail.com>
2023-05-05 19:42:12 -0400

    added confirmation view

---
+++ Sources/AppSDK/Views/ConfirmationView.swift
@@ -0,0 +1,88 @@
+//
+//  ConfirmationView.swift
+//  
+//
+//  Created by Joe Manto on 5/5/23.
+//
+
+import Foundation
+import SwiftUI
+import AppKit
+
+public struct Confirmation {
+    public let title: String
+    public let subtitle: String
+    
+    public init(title: String, subtitle: String) {
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
+
+public class ConfirmationViewModel {
+    public var onContinue: (() -> Void)?
+    public var onCancel: (() -> Void)?
+    
+    public let confirmation: Confirmation
+    
+    public init(confirmation: Confirmation, onContinue: (() -> Void)? = nil, onCancel: (() -> Void)? = nil) {
+        self.onContinue = onContinue
+        self.onCancel = onCancel
+        self.confirmation = confirmation
+    }
+    
+    public func continueTapped() {
+        self.onContinue?()
+    }
+    
+    public func cancelTapped() {
+        self.onCancel?()
+    }
+}
+
+public struct ConfirmationView: View {
+    
+    public let vm: ConfirmationViewModel
+    
+    public init(vm: ConfirmationViewModel) {
+        self.vm = vm
+    }
+    
+    public var body: some View {
+        VStack {
+            Text(vm.confirmation.title)
+                .font(.standardFontMedium(size: 18, relativeTo: .title))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.leading, .top])
+            
+            if !vm.confirmation.subtitle.isEmpty {
+                Text(vm.confirmation.subtitle)
+                    .font(.standardFont(size: 12, relativeTo: .subheadline))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding([.leading])
+            }
+            
+            HStack {
+                Button("Cancel", action: vm.cancelTapped)
+                Spacer()
+                Button("Continue", action: vm.continueTapped)
+            }
+            .padding()
+        }
+        .fixedSize()
+    }
+}
+
+public class ConfirmationViewController: NSHostingController<ConfirmationView> {
+    public func pushToWindow() {
+        let window = NSWindow(contentRect: NSRect.zero, styleMask: [.closable, .titled], backing: .buffered, defer: false)
+        window.contentViewController = self
+        window.makeKeyAndOrderFront(self)
+    }
+}
+
+struct ConfirmationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmationView(vm: ConfirmationViewModel(confirmation: Confirmation(title: "Title", subtitle: "Subtitle")))
+    }
+}
